a90d1f2ac67155cbfcb7a2ab31dc388bef61777e
Fazle Rabbi <fazlerabbi37@gmail.com>
2026-05-25 00:52:38 +0600

    fix formatting

--- scripts/test-urls.sh
+++ scripts/test-urls.sh
@@ -3,23 +3,23 @@
 CF_DOMAIN="tuxstats.com"
 
 URLS=(
-  "https://$CF_DOMAIN/"
-  "https://$CF_DOMAIN/form"
-  "https://$CF_DOMAIN/data"
-  "https://$CF_DOMAIN/result"
-  "https://$CF_DOMAIN/2025/form"
-  "https://$CF_DOMAIN/2025/data"
-  "https://$CF_DOMAIN/2025/result"
+	"https://$CF_DOMAIN/"
+	"https://$CF_DOMAIN/form"
+	"https://$CF_DOMAIN/data"
+	"https://$CF_DOMAIN/result"
+	"https://$CF_DOMAIN/2025/form"
+	"https://$CF_DOMAIN/2025/data"
+	"https://$CF_DOMAIN/2025/result"
 )
 
 for url in "${URLS[@]}"; do
-  echo "--- $url"
-  curl -o /dev/null -s -w "HTTP %{http_code}\n" "$url"
+	echo "--- $url"
+	curl -o /dev/null -s -w "HTTP %{http_code}\n" "$url"
 done
 
 # Uncomment to open each URL in Brave incognito one by one
 # for url in "${URLS[@]}"; do
-#   echo "Opening: $url"
-#   brave-browser-stable --incognito "$url"
-#   read -rp "Press Enter for next..."
+# 	echo "Opening: $url"
+# 	brave-browser-stable --incognito "$url" &>/dev/null
+# 	read -rp "Press Enter for next..."
 # done
